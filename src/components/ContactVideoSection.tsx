dd19fde5e0ff506e086ede1017f8b0ec67f61f60
soheekim <greensohee88@naver.com>
2023-05-14 18:41:31 +0900

    Revert "[1] 동영상을 유튜브에 저장해서 링크만 삽입"
    This reverts commit b5518d221a07dfbd093c6f28183f32612ef848b4.

--- src/components/ContactVideoSection.tsx
+++ src/components/ContactVideoSection.tsx
@@ -17,8 +17,7 @@ const Title = styled.h2`
     padding: 0 0 10px 0
 `;
 
-const Video = styled.iframe` 
-    aspect-ratio: 16 / 9;
+const Video = styled.video` 
     width: 100%;
     border-radius: 8px;
 `;
@@ -28,13 +27,9 @@ export default () => {
     return (
         <Content data-scroll>
             <Title>오리역에서 분당앤스퀘어 가는길</Title>
-            <Video
-                src="https://www.youtube.com/embed/iVZnkDJcwrI"
-                title="오리역에서 분당앤스퀘어 가는길"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen
-                frameBorder="false"
-            />
+            <Video controls>
+                <source src="assets/videos/contact.mp4" type="video/mp4" />
+            </Video>
         </Content>
     )
 };
